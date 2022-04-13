fee9eab99ef0670af6a1657928430e0627f49777
whoismept <mert.umt@hotmail.com>
2022-04-13 15:45:59 +0300

    added configurations

--- parch.sh
+++ parch.sh
@@ -68,3 +68,46 @@ echo "Installing packages..."
 sudo pacman -S ${apps[@]} --noconfirm
 git clone https://aur.archlinux.org/switchboard-plug-pantheon-tweaks-git.git
 (cd switchboard-plug-pantheon-tweaks-git && makepkg -si)
+
+echo "Staring configuration..."
+sudo echo "[Desktop Entry]
+Name=Plank
+Comment=Stupidly simple.
+Exec=plank
+Icon=plank
+Terminal=false
+Type=Application
+Categories=Utility;
+NoDisplay=true
+X-GNOME-Autostart-Notify=false
+X-GNOME-AutoRestart=true
+X-GNOME-Autostart-enabled=true
+X-GNOME-Autostart-Phase=Panel
+OnlyShowIn=Pantheon;
+" > plank.desktop
+
+sudo mv plank.desktop /etc/xdg/autostart/
+echo "Added plank to autostart"
+sudo pacman -S inter-font ttf-opensans ttf-roboto-mono --noconfirm
+gsettings set org.gnome.desktop.interface font-name 'Inter 9'
+gsettings set org.gnome.desktop.interface document-font-name 'Open Sans 10'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Roboto Mono 10'
+echo "Fixed fonts"
+clear
+gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/odin.jpg
+echo "Changed default background"
+sudo sed -i -e '$aHidden=true' /usr/share/applications/bvnc.desktop
+sudo sed -i -e '$aHidden=true' /usr/share/applications/bssh.desktop
+sudo sed -i -e '$aHidden=true' /usr/share/applications/avahi-discover.desktop
+sudo sed -i -e '$aHidden=true' /usr/share/applications/qv4l2.desktop
+sudo sed -i -e '$aHidden=true' /usr/share/applications/qvidcap.desktop
+sudo sed -i -e '$aHidden=true' /usr/share/applications/gda-browser-5.0.desktop
+sudo sed -i -e '$aHidden=true' /usr/share/applications/gda-control-center-5.0.desktop
+sudo sed -i -e '$aHidden=true' /usr/share/applications/plank.desktop
+echo "Hidden unnecessary applications"
+sudo sed -i '102i\greeter-session=io.elementary.greeter' /etc/lightdm/lightdm.conf
+sudo systemctl enable lightdm
+echo "Configured LightDM and added to start"
+echo "Everything is done!"
+echo "Reboot to apply changes"
+echo "Enjoy!"
